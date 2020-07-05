486ee970cd81a46a93f6ec8f59e888678bc2b1fb
Luc Vauvillier <luc.vauvillier@gmail.com>
2020-07-05 18:17:38 +0200

    feat(types): enforce typings for rule definitions

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,21 @@
-import { IRules } from 'graphql-shield'
-import { IOptionsConstructor } from 'graphql-shield/dist/types'
+import { IOptionsConstructor, ShieldRule } from 'graphql-shield/dist/types'
+import { GetGen } from '@nexus/schema/dist/core'
+
+type GenTypes = Extract<keyof GetGen<'fieldTypes'>, string>
+type GenFields<T extends GenTypes> = Extract<
+  keyof GetGen<'fieldTypes'>[T],
+  string
+>
+
+export type IRuleTypeMap = {
+  [T in GenTypes]?: ShieldRule | IRuleFieldMap<T>
+}
+export type IRuleFieldMap<T extends GenTypes> = {
+  [F in GenFields<T>]?: ShieldRule
+} & {
+  '*'?: ShieldRule
+}
+export declare type IRules = ShieldRule | IRuleTypeMap
 
 export interface Settings {
   rules: IRules
